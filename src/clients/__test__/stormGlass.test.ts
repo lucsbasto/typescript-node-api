06603c0ba449bd23803cc288b8ab1d20ae885073
Lucas Bastos <lucsbasto@gmail.com>
2020-08-25 10:57:54 -0300

    mock axios to typescript can see all axios methods

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,13 +6,12 @@ import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normali
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
   it('should retorn the normalized forecast from the StormGlass service', async () => {
     const lat = -33.784515;
     const lng = 151.254841;
-    axios.get = jest
-      .fn()
-      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
-    const stormGlass = new StormGlass(axios);
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+    const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormGlassNormalized3HoursFixture);
   });
