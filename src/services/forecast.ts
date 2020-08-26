e067836ee985e76085008b7700d557b23bb782a9
Lucas Bastos <lucsbasto@gmail.com>
2020-08-26 14:05:11 -0300

    add tests and implementation to group forecast by hour

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -19,10 +19,15 @@ export interface Beach{
 
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
+export interface TimeForecast {
+    time: string;
+    forecast: BeachForecast[];
+}
+
 export class Forecast {
     constructor(protected stormGlass= new StormGlass()){}
 
-    public async processForecastForBeaches(beaches: Beach[]): Promise<BeachForecast[]>{
+    public async processForecastForBeaches(beaches: Beach[]): Promise<TimeForecast[]>{
         const pointsWithCorrectSources: BeachForecast[] = []
         for (const beach of beaches){
             const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
@@ -38,6 +43,22 @@ export class Forecast {
             }));
             pointsWithCorrectSources.push(...enrichedBeachData);
         }
-        return pointsWithCorrectSources
+        return this.mapForecastByTime(pointsWithCorrectSources)
+    }
+
+    private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[]{
+        const forecastByTime: TimeForecast[] = []
+        for(const point of forecast){
+            const timeForecast = forecastByTime.find((f) => f.time === point.time);
+            if (timeForecast){
+                timeForecast.forecast.push(point);
+            }else{
+                forecastByTime.push({
+                    time: point.time,
+                    forecast: [point]
+                });
+            }
+        }
+        return forecastByTime;
     }
 }
