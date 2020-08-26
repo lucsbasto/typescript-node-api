bb089d2230b73442487e52619288900937497c9e
Lucas Bastos <lucsbasto@gmail.com>
2020-08-26 17:33:24 -0300

    add controller and tests to create a new beach

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
+import { BeachesController } from './controllers/beaches';
 import { Application } from 'express';
-import * as database from '@src/database'
+import * as database from '@src/database';
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
@@ -21,15 +22,16 @@ export class SetupServer extends Server {
 
   private setupControllers(): void {
     const forecastController = new ForecastController();
-    this.addControllers([forecastController]);
+    const beachesController = new BeachesController();
+    this.addControllers([forecastController, beachesController]);
   }
 
-  private  async databaseSetup(): Promise<void> {
+  private async databaseSetup(): Promise<void> {
     await database.connect();
   }
 
-  public async close(): Promise<void>{
-    await database.close()
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   public getApp(): Application {
