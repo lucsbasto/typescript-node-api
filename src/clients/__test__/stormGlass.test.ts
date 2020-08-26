76d85ee79da3f558dce319458e99a44294569374
Lucas Bastos <lucsbasto@gmail.com>
2020-08-26 10:24:09 -0300

    add tests and class StormGlassResponseError to deal with StormGlass errors

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -46,4 +46,21 @@ describe('StormGlass client', () => {
       'Unexpected error when trying to communicate to StormGlass: Network Error'
     );
   });
+
+  it('should get a StormGlassResponseError when the StormGlass responds with error', async () => {
+    const lat = -33.784515;
+    const lng = 151.254841;
+    mockedAxios.get.mockRejectedValue({
+      response: {
+        status: 429,
+        data: { errors: ['Rate Limit reached'] },
+      },
+    });
+
+    const stormGlass = new StormGlass(mockedAxios);
+
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      'Unexpected error returned by the StormGlass service: Error: {"errors":["Rate Limit reached"]} Code: 429'
+    );
+  });
 });
